8132547acc175e270b2e86ab8b9a1961e73bedd8
Viktoriya Datchuk <grelada@tut.by>
2023-10-19 14:45:49 +0300

    feat: create component, add button and icon

--- DucuScetch-test/src/app/app.module.ts
+++ DucuScetch-test/src/app/app.module.ts
@@ -4,17 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MyComponentComponent } from './my-component/my-component.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, MyComponentComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ DucuScetch-test/src/app/my-component/my-component.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  IconDefinition,
+  IconName,
+  fas,
+} from '@fortawesome/free-solid-svg-icons';
+import { findIconDefinition } from '@fortawesome/fontawesome-svg-core';
+
+@Component({
+  selector: 'app-my-component',
+  templateUrl: './my-component.component.html',
+  styleUrls: ['./my-component.component.scss'],
+})
+export class MyComponentComponent {
+  icons: Array<string>;
+  length: number;
+  isIcon = false;
+  myIcon!: IconDefinition;
+
+  constructor() {
+    library.add(fas);
+    this.icons = Object.keys(
+      Object.values(Object.values(library)[0])[0] as Object
+    );
+    this.length = this.icons.length;
+  }
+
+  randomNumber(min: number, max: number) {
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
+  }
+
+  buttonClickHandler() {
+    this.isIcon = true;
+    let iconNumber = this.randomNumber(0, this.length - 1);
+    this.myIcon = findIconDefinition({
+      prefix: 'fas',
+      iconName: this.icons[iconNumber] as IconName,
+    });
+  }
+}
